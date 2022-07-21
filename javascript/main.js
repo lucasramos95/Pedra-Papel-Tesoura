0ac7ba5be9c7c6b4ba50838f21dce074998bdac0
Lucas Ramos <108039029+lucasramos95@users.noreply.github.com>
2022-07-21 16:22:46 -0300

    5th Commit - HTML e CSS modificado + JavaScript

---
+++ javascript/main.js
@@ -0,0 +1,15 @@
+//Lógica do jogo, na função abaixo
+const placar = () => {
+    let pontosJogador = 0; 
+    let pontosCpu = 0;
+    console.log();
+}
+
+const jogarJogo = () => {
+    const pedraBtn = document.querySelector('.pedra');
+    const papelBtn = document.querySelector('.papel');
+    const tesouraBtn = document.querySelector('.tesoura');
+    const opcoesJogador = [pedraBtn, papelBtn, tesouraBtn];
+    const opcoesCpu = ['pedra','papel','tesoura']
+    console.log();
+}
