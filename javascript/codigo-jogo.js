fae8ece9c2e8c1c7c6ad86252e942884b6182f34
Lucas Ramos <108039029+lucasramos95@users.noreply.github.com>
2022-07-24 11:11:49 -0300

    javascript/codigo-jogo - Modificado

--- javascript/codigo-jogo.js
+++ javascript/codigo-jogo.js
@@ -32,5 +32,48 @@ function jogarTurno(event) {
     obterResultado(selecaoCpuBtn,selecaoJogadorBtn);
 }
 
+//jogar o round quando o botão de jogo é clicado
+for(let elem of elementosDoJogo.jogoBotoes) {
+    elem.addEventListener('click', jogarTurno);
+}
 
+//comparar seleções e obter resultado
+function obterResultado(selecaoJogadorBtn, selecaoCpuBtn) {
+    if (selecaoJogadorBtn === selecaoCpuBtn) {
+        resultado = undefined;
+    } else if (selecaoJogadorBtn === 'pedra-btn' && selecaoCpuBtn === 'pedra-btn') {
+        resultado = true;
+    } else if (selecaoJogadorBtn === 'pedra-btn' && selecaoCpuBtn === 'tesoura-btn') {
+        result = true;
+    } else if (selecaoJogadorBtn === 'tesoura-btn' && selecaoCpuBtn === 'papel-btn') {
+        result = true;
+    } else {
+        resultado = false;
+    }
+        return resultado;
+}
 
+//''printar'' resultado e acrescer placar para vitória, decrescer placar para derrota
+function printarResultado() {
+    switch(resultado) {
+        case undefined:
+            elementosDoJogo.resultadoJogo.textContent = 'Empate.';
+            break;
+        case true:
+            elementosDoJogo.selecaoJogadorBtn.classList.add('é o vencedor');
+            elementosDoJogo.resultadoJogo.textContent = 'Você Venceu!';
+            placar++;
+            elementosDoJogo.jogoPlacar.textContent = placar;
+            break;
+        case false:
+            elementosDoJogo.resultadoJogo.textContent = 'Você Perdeu';
+            elementosDoJogo.selecaoCpuBtn.classList.add('é o vencedor');
+            placar--;
+            elementosDoJogo.jogoPlacar.textContent = placar;
+            break;
+    }
+    console.log(`result: ${elementosDoJogo.resultadoJogo.textContent}`);
+
+    //manter o placar do jogo
+    localStorage.setItem('placar', JSON.stringify(placar));
+}
